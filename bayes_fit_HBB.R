4318b6dce20dfef715c8181bbc67adb7783703bb
cfj1998 <61003849+cfj1998@users.noreply.github.com>
2025-05-23 11:06:37 +0200

    Add files via upload

---
+++ bayes_fit_HBB.R
@@ -0,0 +1,39 @@
+#Bayesian Interval in the non risk HBB setting
+#edited by me
+#Orignial Code provided to me by Jan Terje Kvaloy
+rm(list = ls())
+
+#prior distribution
+alpha0 <- 3
+
+beta0 <- 200
+
+curve(dbeta(x,shape1 = alpha0, shape2 = beta0),from=0,to=1,
+      
+      ylab="f(x)", col="red")
+
+
+alpha0/(alpha0+beta0)
+
+# Quantiles, prior distribution
+
+qbeta(0.025,shape1 = alpha0, shape2 = beta0)
+
+qbeta(0.975,shape1 = alpha0, shape2 = beta0)
+
+#Baseline information
+nbaseline <- 4839
+
+sumybaseline <- 133
+
+# Posterior mean
+
+(sumybaseline+alpha0)/(nbaseline+alpha0+beta0)
+
+
+
+# Bayes interval
+
+qbeta(0.025,shape1 = sumybaseline+alpha0, shape2 = nbaseline-sumybaseline+beta0)
+
+qbeta(0.975,shape1 = sumybaseline+alpha0, shape2 = nbaseline-sumybaseline+beta0)
